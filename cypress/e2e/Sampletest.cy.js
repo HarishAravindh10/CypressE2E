fb9a2f932d176e4b22c3f928f3b5ac0d0fcbd04f
HarishAravindh10 <159517413+HarishAravindh10@users.noreply.github.com>
2024-03-20 18:35:35 +0530

    Update Sampletest.cy.js

--- cypress/e2e/Sampletest.cy.js
+++ cypress/e2e/Sampletest.cy.js
@@ -2,7 +2,7 @@ describe('template spec', () =>
 {
   it('passes', () => 
   {
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:3002/')
     cy.title().should('eq','React App')
 
     cy.get("#username").type('admin')
